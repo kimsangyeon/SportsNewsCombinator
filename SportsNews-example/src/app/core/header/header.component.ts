a1bef517080c7c48cf8559c0a6219a5ba6be0629
Kim Sangyeon <dho113@naver.com>
2018-09-06 00:09:17 +0900

    header.component.ts 기본 틀 구성

--- SportsNews-example/src/app/core/header/header.component.ts
+++ SportsNews-example/src/app/core/header/header.component.ts
@@ -1,3 +1,12 @@
-/**
- * Created by kimsangyeon on 2018. 9. 6..
- */
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'snc-header',
+  templateUrl: './heder.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
