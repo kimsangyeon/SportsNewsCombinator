09c7d1455188646dd03289292198375837f8e8b9
Kim Sangyeon <dho113@naver.com>
2018-09-09 10:16:34 +0900

    import moudule news service api

--- SportsNews-example/src/app/app.module.ts
+++ SportsNews-example/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { NewsComponent } from './dashboard/news/news.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 
+import { NewsapiService } from './service/newsapi.service';
+
 @NgModule({
   declarations: [  // 모듈에 속한 component 목록
     AppComponent,
@@ -16,7 +18,7 @@ import { FooterComponent } from './core/footer/footer.component';
   imports: [  // 외부 모듈
     BrowserModule
   ],
-  providers: [],  // 참조하려는 서비스
+  providers: [NewsapiService],  // 참조하려는 서비스
   bootstrap: [AppComponent] // 시작 컴포넌트
 })
 export class AppModule { }
--- SportsNews-example/src/app/service/newsapi.service.ts
+++ SportsNews-example/src/app/service/newsapi.service.ts
@@ -1,3 +1,9 @@
-export class NewsapiService {
+import {Injectable} from '@angular/core';
 
+@Injectable()
+export class NewsapiService {
+  constructor() {}
+
+  // 웹서버에서 데이터를 가져오는 역할
+  public fecthNewsFeed() {}
 }
