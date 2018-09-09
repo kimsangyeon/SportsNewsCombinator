49f3089b0e9cbc32bef549bb50d3e1487d198b9f
Kim Sangyeon <dho113@naver.com>
2018-09-09 10:57:26 +0900

    route module import

--- SportsNews-example/src/app/app.module.ts
+++ SportsNews-example/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NewsComponent } from './dashboard/news/news.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
+import {AppComponent} from './app.component';
+import {NewsComponent} from './dashboard/news/news.component';
+import {HeaderComponent} from './core/header/header.component';
+import {FooterComponent} from './core/footer/footer.component';
 
-import { NewsapiService } from './service/newsapi.service';
+import {NewsapiService} from './service/newsapi.service';
+import {newsroutingmodule} from "./app.route";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [  // 모듈에 속한 component 목록
@@ -16,7 +18,9 @@ import { NewsapiService } from './service/newsapi.service';
     FooterComponent
   ],
   imports: [  // 외부 모듈
-    BrowserModule
+    BrowserModule,
+    newsroutingmodule,
+    HttpModule
   ],
   providers: [NewsapiService],  // 참조하려는 서비스
   bootstrap: [AppComponent] // 시작 컴포넌트
